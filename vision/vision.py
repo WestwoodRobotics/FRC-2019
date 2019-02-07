0f810ea01dcf581b95e4b1bc3f6bdd4a59cc5871
Locke <Artinaire@gmail.com>
2019-02-07 15:09:39 -0600

    Change from dash to vision, detects a hatch succesfully.

---
+++ vision/vision.py
@@ -0,0 +1,93 @@
+from networktables import NetworkTables
+import numpy as np
+import click
+import cv2
+
+from time import perf_counter
+import collections
+import math
+
+def get_color_mask(image):
+  hsv = cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
+  cv2.medianBlur(hsv, 5, hsv)
+  
+  halfW = hsv.shape[1] // 2
+  maskL = cv2.inRange(hsv[:, :halfW], minColor[0], maxColor[0])
+  maskR = cv2.inRange(hsv[:, halfW:], minColor[1], maxColor[1])
+  mask = np.hstack((maskL, maskR))
+    
+  return mask, hsv 
+
+def find_image_location(method, field_image, template):
+  img_B, img_G, img_R = cv2.split(field_image)
+
+  grey_field_image = cv2.cvtColor(field_image, cv2.COLOR_BGR2GRAY) # !! 
+  res = cv2.matchTemplate(grey_field_image, template, method)
+
+  min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+  # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+  if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+    top_left = min_loc
+  else:
+    top_left = max_loc
+
+  w, h = cv2.cvtColor(field_image, cv2.COLOR_BGR2GRAY).shape[::-1] 
+  bottom_right = (top_left[0] + w, top_left[1] + h)
+  res = cv2.rectangle(field_image, top_left, bottom_right, 255, 2)
+  
+  cv2.imshow("Found Image", res)
+
+  # return cv2.minAreaRect(res)
+
+def find_hatch(method, field_image, hatch_image="hatch.jpg"):
+  return find_image_location(method, field_image, cv2.imread(hatch_image, 0))
+
+def find_hatch_placement(method, field_image, hatch_placement_image="hatch_location.jpg"):
+  return find_image_location(method, field_image, cv2.imread(hatch_placement_image, 0))
+
+def find_hatch_distance():
+  pass
+
+def run(smartDashboard, cap):  
+  KNOWN_DISTANCE = 24.0
+  KNOWN_WIDTH = 11.0
+  METHOD = cv2.TM_CCOEFF_NORMED
+
+  while cap.isOpened():
+    ret, frame = cap.read()
+    print(frame)
+    cv2.imshow("preview", frame)
+    cv2.waitKey()
+
+    find_hatch(METHOD, field_image=frame)
+
+def init_image_capture(device):
+  cap = cv2.VideoCapture(device)
+  if not cap.isOpened():
+    print("failed to open camera device {0}".format(device))
+    if device != 0:
+      print("resetting device id to 0")
+      return init_image_capture(0) 
+
+  # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1400)
+  # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1400)
+  print("done initializing the image capture")
+
+  return cap
+
+@click.command()
+@click.option("--ip", default="10.2.54.2", help="Default ip to connect to")
+@click.option("--network", default=True, help="Whether to connect with a network")
+@click.option("--device", default=0, help="Device id used to connect to")
+def main(ip, network, device):
+  smartDashboard = None
+  if network:
+    NetworkTables.initialize(server=ip)
+    smartDashboard = NetworkTables.getTable("SmartDashboard")
+
+  cap = init_image_capture(device)
+  run(smartDashboard, cap)
+
+if __name__ == '__main__':
+  main()
